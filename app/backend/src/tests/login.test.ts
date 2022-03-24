5010c613203cf5e64bd16a24fb986b882ec85c53
dandara-dias <dandaradias.contato@gmail.com>
2022-03-24 19:46:47 -0300

    Requisito 4 - teste1

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,70 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/User';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Requisito 5', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
+
+  // let chaiHttpResponse: Response;
+
+  // before(async () => {
+  //   sinon
+  //     .stub(Example, "findOne")
+  //     .resolves({
+  //       ...<Seu mock>
+  //     } as Example);
+  // });
+
+  // after(()=>{
+  //   (Example.findOne as sinon.SinonStub).restore();
+  // })
+
+  // it('...', async () => {
+  //   chaiHttpResponse = await chai
+  //      .request(app)
+  //      ...
+
+  //   expect(...)
+  // });
+
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      } as User);
+  });
+
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+  it('O avaliador verificará se é possível fazer o login com dados corretos', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'admin@admin.com',
+        password: 'secret_admin',
+      });
+
+    expect(chaiHttpResponse.status).to.equal(200);
+  });
+});
