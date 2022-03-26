202327adee24f53d1f58502489ae163f64357dc4
dandara-dias <dandaradias.contato@gmail.com>
2022-03-26 19:04:16 -0300

    Requisito 12 - teste1

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Requisitos 5, 7, 9 e 11', () => {
+describe('Requisitos 5, 7, 9, 11 e 13', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -114,6 +114,57 @@ describe('Requisitos 5, 7, 9 e 11', () => {
       })
       .then(function (res){
         expect(res).to.have.status(401);
+      })
+      .catch(function (err) {
+        throw err;
+      });
+  });
+
+  it('O avaliador verificará se ao tentar fazer login sem senha retornará status não-autorizado', async () => {
+    chai
+      .request(app)
+      .post('/login')
+      .send({
+        email: 'admin@admin.com',
+        password: '',
+      })
+      .then(function (res){
+        expect(res).to.have.status(401);
+      })
+      .catch(function (err) {
+        throw err;
+      });
+  });
+});
+
+describe('Requisito 14', () => {
+  let chaiHttpResponse: Response;
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves({
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      } as User);
+  });
+
+  after(() => {
+    (User.findOne as sinon.SinonStub).restore();
+  });
+
+  it('O avaliador verificará se tentar bater na rota com um token válido, o mesmo retornará o tipo de usuário', async () => {
+    chai
+      .request(app)
+      .get('/login/validate')
+      .set({
+        authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkFkbWluIiwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJwYXNzd29yZCI6IiQyYSQwOCR4aS5IeGsxY3pBTzBuWlIuLkIzOTN1MTBhRUQwUlExTjNQQUVYUTdIeHRMaktQRVpCdS5QVyJ9LCJpYXQiOjE2NDgzMjM2MDAsImV4cCI6MTY0ODkyODQwMH0.4yB0P7foau8rTAQsnU-sZcxniGlS4Nk-RY9JvO2aFCs'
+      })
+      .then(function (res){
+        expect(res).to.have.status(200);
       })
       .catch(function (err) {
         throw err;
