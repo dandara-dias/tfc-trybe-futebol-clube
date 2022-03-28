14ddcdfcaa36f396471435846c8d31844670530d
dandara-dias <dandaradias.contato@gmail.com>
2022-03-28 12:01:58 -0300

    Requisitos 20 e 21 - teste1

--- app/backend/src/Controllers/matchController.ts
+++ app/backend/src/Controllers/matchController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
-import getMatches from '../Services/matchService';
+import { getMatches, getInProgressMatches, getFinishedMatches } from '../Services/matchService';
 
 const matchController = async (req: Request, res: Response) => {
+  const { inProgress } = req.query;
+
+  if (inProgress === 'true') {
+    const progress = await getInProgressMatches();
+    return res.status(progress.status).json(progress.message);
+  }
+
+  if (inProgress === 'false') {
+    const finished = await getFinishedMatches();
+    return res.status(finished.status).json(finished.message);
+  }
+
   const getAllMatches = await getMatches();
 
   res.status(getAllMatches.status).json(getAllMatches.message);
--- app/backend/src/Services/matchService.ts
+++ app/backend/src/Services/matchService.ts
@@ -23,4 +23,58 @@ const getMatches = async () => {
   }
 };
 
-export default getMatches;
+const getInProgressMatches = async () => {
+  const matches = await Match.findAll({
+    include: [
+      {
+        model: Club,
+        as: 'homeClub',
+        attributes: ['clubName'],
+      },
+      {
+        model: Club,
+        as: 'awayClub',
+        attributes: ['clubName'],
+      },
+    ],
+    where: {
+      inProgress: 1,
+    },
+  });
+
+  return {
+    message: matches,
+    status: 200,
+  }
+};
+
+const getFinishedMatches = async () => {
+  const matches = await Match.findAll({
+    include: [
+      {
+        model: Club,
+        as: 'homeClub',
+        attributes: ['clubName'],
+      },
+      {
+        model: Club,
+        as: 'awayClub',
+        attributes: ['clubName'],
+      },
+    ],
+    where: {
+      inProgress: 0,
+    },
+  });
+
+  return {
+    message: matches,
+    status: 200,
+  }
+};
+
+export {
+  getMatches,
+  getInProgressMatches,
+  getFinishedMatches,
+};
